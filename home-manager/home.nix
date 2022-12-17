972f6c638f3a020f1f3cc7be0e2e30ca516c79f7
Leon Breedt <leon@sector42.io>
2022-12-17 14:13:44 +1300

    fix: remove gpg-agent, only works on Linux

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -93,12 +93,4 @@
   };
 
   programs.gpg.enable = true;
-  services.gpg-agent = {
-    enable = true;
-    pinentryFlavor = "tty";
-
-    # cache the keys forever so we don't get asked for a password
-    defaultCacheTtl = 31536000;
-    maxCacheTtl = 31536000;
-  };
 }
