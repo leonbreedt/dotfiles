6f50433092b4a73ed4e151f04d5d8a3906db5f0e
Leon Breedt <leon@sector42.io>
2022-12-17 14:18:18 +1300

    feat: add popui and catppuccin

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -52,7 +52,9 @@
     viAlias = true;
     vimAlias = true;
     plugins = with pkgs; [
+      # Theme/appearance
       vimPlugins.lightline-vim
+      localVimPlugins.catppuccin-nvim
 
       # LSP
       vimPlugins.nvim-lspconfig
@@ -72,6 +74,7 @@
 
       # popups
       vimPlugins.popfix
+      localVimPlugins.popui-nvim
 
       # tree sitter
       (vimPlugins.nvim-treesitter.withPlugins (plugins: with plugins; [
@@ -93,4 +96,32 @@
   };
 
   programs.gpg.enable = true;
+
+  nixpkgs.overlays = [
+    (self: super:
+      {
+        localVimPlugins = with self; {
+          catppuccin-nvim = vimUtils.buildVimPlugin {
+            name = "catppuccin-nvim";
+            src = fetchFromGitHub {
+              owner = "catppuccin";
+              repo = "nvim";
+              rev = "cd676faa020b34e6617398835b5fa3d1c2e8895c";
+              sha256 = "00lww24cdnayclxx4kkv19vjysdw1qvngrf23w53v6x4f08s24my";
+            };
+          };
+
+          popui-nvim = vimUtils.buildVimPlugin {
+            name = "popui-nvim";
+            src = fetchFromGitHub {
+              owner = "hood";
+              repo = "popui.nvim";
+              rev = "5836baf9514f1a463e6617b5ea72669b597f8259";
+              sha256 = "1ga4n5b7p4v96nnfjbmy0b6zzacwnp2axyqmckgd52wn3lg407q9";
+            };
+          };
+        };
+      }
+    )
+  ];
 }
